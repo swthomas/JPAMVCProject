0bae28374e37e0073dd5b6634219e586b4b76de6
Seth Thomas <swthomas34@gmail.com>
2017-04-03 10:39:45 -0600

    tests

--- MVCMidtermProject/src/controllers/CreateController.java
+++ MVCMidtermProject/src/controllers/CreateController.java
@@ -1,5 +1,42 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import data.FamilyDAO;
+import data.MemberDAO;
+import entities.Family;
+
+@Controller
+@SessionAttributes("user")
 public class CreateController {
+	@Autowired
+	private MemberDAO memberDao;
+	
+	@Autowired FamilyDAO familyDao;
+
+	
+	@RequestMapping(path = "CreateFamily.do", method = RequestMethod.POST)
+	public void createFamily() {
+		ModelAndView mv = new ModelAndView();
+		Family f = familyDao.createFamily();
+		createMembers(f);
+		
+		if(colleges.size()==0){
+	    	mv.setViewName("error");
+	    }
+	    else{
+	    	mv.setViewName("home");
+	    	mv.addObject("collegeList", colleges);
+	    }
+		
+		
+		
+	}
+	
 
 }
