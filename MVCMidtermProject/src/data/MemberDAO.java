b8492b814e77cf81f58f62e5c8a247e7827f774e
Seth Thomas <swthomas34@gmail.com>
2017-04-04 11:18:22 -0600

    add member daothingy

--- MVCMidtermProject/src/data/MemberDAO.java
+++ MVCMidtermProject/src/data/MemberDAO.java
@@ -6,7 +6,8 @@ import entities.Family;
 import entities.Member;
 
 public interface MemberDAO {
-	List<Member> createMembersList(List<Member> memberList, Family family);
+	public List<Member> createMembersList(List<Member> memberList, Family family);
+	public Member createMember(Member member, Family family);
 	public Member updateMember(Member member);
 	public Member createMember(Member member);
 	public Member showMember(int id);
