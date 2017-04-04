e4b263f68f3258f8ea05b5f51e4cfe2a2c6f59f7
Greg <gmengler@gmail.com>
2017-04-04 14:38:22 -0600

    controller changes and edituserbill added

--- MVCMidtermProject/src/controllers/EditController.java
+++ MVCMidtermProject/src/controllers/EditController.java
@@ -10,8 +10,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import data.AccountDAO;
 import data.BillDAO;
-import entities.Account;
+import data.MemberDAO;
 import entities.Bill;
+import entities.Member;
 
 @Controller
 @SessionAttributes("sessionUser")
@@ -22,6 +23,9 @@ public class EditController {
 	
 	@Autowired
 	BillDAO billdao;
+
+	@Autowired
+	MemberDAO memberdao;
 	
 //	@RequestMapping(path = "SetBankAccount.do", method = RequestMethod.POST)
 //	public ModelAndView SetAccount(@ModelAttribute("sessionUser") Account a) {
@@ -34,6 +38,16 @@ public class EditController {
 //		return mv;
 //	}
 	
+	@RequestMapping(path = "GetBill.do", method = RequestMethod.POST)
+	public ModelAndView getBill(@ModelAttribute("sessionUser") Bill bill) {
+		ModelAndView mv = new ModelAndView();
+		billdao.getBill(bill.getId());
+		mv.addObject("bill", bill);
+		mv.setViewName("editbill");
+
+		return mv;
+	}
+
 	@RequestMapping(path = "EditUserBill.do", method = RequestMethod.POST)
 	public ModelAndView editUserBill(@ModelAttribute("sessionUser") Bill bill) {
 		ModelAndView mv = new ModelAndView();
@@ -43,34 +57,33 @@ public class EditController {
 		
 		return mv;
 	}
-	
-	@RequestMapping(path = "GetBill.do", method = RequestMethod.POST)
-	public ModelAndView getBill(@ModelAttribute("sessionUser") Bill bill) {
-		ModelAndView mv = new ModelAndView();
-		billdao.getBill(bill.getId());
-		mv.addObject("bill", bill);
-		mv.setViewName("editbill");
-		
-		return mv;
-	}
-	
+
 	@RequestMapping(path = "EditAdminBill.do", method = RequestMethod.POST)
 	public ModelAndView editAdminBill(@ModelAttribute("sessionUser") Bill bill) {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("bill", billdao.getBill(bill.getId()));
 		mv.setViewName("editadminbill");
-		
 		return mv;
 	}
-	
+
 	@RequestMapping(path = "EditAdminBillFields.do", method = RequestMethod.POST)
-	public ModelAndView editBeer(Bill bill){
+	public ModelAndView editAdminFields(@ModelAttribute("sessionUser") Member member, Bill bill) {
 		billdao.updateBill(bill);
+		Member m = memberdao.showMember(member.getId());
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("billlist", billdao.getFamilyBills(bill.getId()));
-		mv.addObject("billlist", billdao.getMemberBills(bill.getId()));
+		mv.addObject("member", m);
 		mv.setViewName("adminProfile");
 		return mv;
-		}
+	}
+
+	@RequestMapping(path = "EditUserBillFields.do", method = RequestMethod.POST)
+	public ModelAndView editUserFields(@ModelAttribute("sessionUser") Member member, Bill bill) {
+		billdao.updateBill(bill);
+		Member m = memberdao.showMember(member.getId());
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("member", m);
+		mv.setViewName("userProfile");
+		return mv;
+	}
 
 }
