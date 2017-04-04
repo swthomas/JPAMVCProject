13746d183ebcc248a29fc281acc5d8c37936aab6
Seth Thomas <swthomas34@gmail.com>
2017-04-04 12:30:53 -0600

    asdf

--- MVCMidtermProject/src/data/MemberDAO.java
+++ MVCMidtermProject/src/data/MemberDAO.java
@@ -9,7 +9,6 @@ public interface MemberDAO {
 	public List<Member> createMembersList(List<Member> memberList, Family family);
 	public Member createMember(Member member, Family family);
 	public Member updateMember(Member member);
-	public Member createMember(Member member);
 	public Member showMember(int id);
 	public boolean deleteMember(int id);
 }
--- MVCMidtermProject/src/data/MemberDAOImpl.java
+++ MVCMidtermProject/src/data/MemberDAOImpl.java
@@ -47,7 +47,7 @@ public class MemberDAOImpl implements MemberDAO{
 
 	
 	@Override
-	public Member createMember(Member member) {		
+	public Member createMember(Member member, Family family) {		
 		Member m = new Member();
 		m = member;
 		
@@ -57,6 +57,8 @@ public class MemberDAOImpl implements MemberDAO{
 		
 		m.setAccount(a);
 		a.setMember(m);
+		
+		member.setFamily(family);
 		
 		em.persist(m);
 		em.flush();
